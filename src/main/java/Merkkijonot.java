b9612a8d299d04817d167bab5865c95b5a792f94
kirsi-k <kirsikujala@hotmail.com>
2019-06-13 14:42:20 +0300

    Päivitetty merkkijonoharjoitus i

--- src/main/java/Merkkijonot.java
+++ src/main/java/Merkkijonot.java
@@ -2,22 +2,19 @@ import java.util.Arrays;
 
 public class Merkkijonot {
 
-    public Merkkijonot(){
+    public Merkkijonot() {
 
     }
-    public boolean onkoPalindromi(String sana){
+
+    public boolean onkoPalindromi(String sana) {
         return sana.equals(new StringBuilder(sana).reverse().toString());
     }
-    public boolean onkoAnagrammi (String ekasana, String tokasana){
-        char[] sana1 = ekasana.replaceAll("[\\s]", "").toCharArray();
-        char[] sana2 = tokasana.replaceAll("[\\s]", "").toCharArray();
+
+    public boolean onkoAnagrammi(String ekasana, String tokasana) {
+        char[] sana1 = ekasana.toCharArray();
+        char[] sana2 = tokasana.toCharArray();
         Arrays.sort(sana1);
         Arrays.sort(sana2);
         return Arrays.equals(sana1, sana2);
-    }
-    public String generoiKontinKieleen(String sana){
-        StringBuilder kontiksi = new StringBuilder();
-        kontiksi.append("kontti");
-        return null;
     }
 }
--- src/test/java/MerkkijonotTest.java
+++ src/test/java/MerkkijonotTest.java
@@ -4,29 +4,22 @@ import static org.junit.Assert.*;
 
 public class MerkkijonotTest {
 
+    Merkkijonot m = new Merkkijonot();
+
     @Test
     public void TestaaOnkoSanaPalindromi() {
-        Merkkijonot m = new Merkkijonot();
         String alkuperainenSana = "saippuakauppias";
         boolean tulos = true;
         boolean palindromi = m.onkoPalindromi(alkuperainenSana);
         assertEquals(tulos, palindromi);
     }
+
     @Test
-    public void TestaaOnkoSanaAnagrammi(){
-        Merkkijonot m = new Merkkijonot();
+    public void TestaaOnkoSanaAnagrammi() {
         String ekasana = "lista";
         String tokasana = "silta";
         boolean tulos = true;
         boolean anagrammi = m.onkoAnagrammi(ekasana, tokasana);
         assertEquals(tulos, anagrammi);
-    }
-    @Test
-    public void TestaaToimiikoKontinKieli(){
-        Merkkijonot m = new Merkkijonot();
-        String sana = "yksi";
-        String konttisana = m.generoiKontinKieleen(sana);
-        String tulos = "koksi yntti";
-        assertEquals(tulos, konttisana);
     }
 }
